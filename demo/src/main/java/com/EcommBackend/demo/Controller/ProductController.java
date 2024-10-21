8968ec90e75188f139c06ebe408a95e52fe508d9
gideonadjei <gideonadjei94@gmail.com>
2024-10-21 20:59:35 +0000

    Get Product API

--- demo/src/main/java/com/EcommBackend/demo/Controller/ProductController.java
+++ demo/src/main/java/com/EcommBackend/demo/Controller/ProductController.java
@@ -3,6 +3,8 @@ package com.EcommBackend.demo.Controller;
 import com.EcommBackend.demo.Model.Product;
 import com.EcommBackend.demo.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +19,18 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> Products = service.getAllProducts();
+        return new ResponseEntity<>(Products, HttpStatus.OK);
     }
 
     @GetMapping("/product/{id}")
-    public Optional<Product> getProduct(@PathVariable int id){
-        return service.getProductById(id);
+    public ResponseEntity<Optional<Product>> getProduct(@PathVariable int id){
+        Optional<Product> product = service.getProductById(id);
+        if(product != null){
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }else{
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
